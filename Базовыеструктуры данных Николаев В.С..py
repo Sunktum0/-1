607cea55e5112a7f85cdb0d3c0493769ccafa96e
Sunktum0 <vlad99-2010@yandex.ru>
2024-07-13 10:07:57 +0300

    Add files via upload

---
+++ Базовыеструктуры данных Николаев В.С..py
@@ -0,0 +1,9 @@
+print ((9**0.5)*5) # "1st program" 
+print (9.99>9.98 and 1000!=1000.1) # "2nd program"
+print (1234//10-100+5678//10-500) #"3rd program"
+print (int (13.42)) #"4th program"
+print (int(((42.13-(int(42.13))))*100))
+print ((int (13.42))==(int(((42.13-(int(42.13))))*100)))
+print (int(13.42*100%100))
+print ((int(42.13))==(int(13.42*100%100)))
+print (((int (13.42))==(int(((42.13-(int(42.13))))*100)))and((int(42.13))==(int(13.42*100%100))))
